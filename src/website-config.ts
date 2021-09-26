77970455c3867dcacfb7805dd17f8ce55a477af8
zlrlo <easy10612@gmail.com>
2021-09-26 16:54:35 +0900

    chore: website-config 수정
    - siteUrl 수정
    - mailchimp 관련 기능 주석 처리

--- src/website-config.ts
+++ src/website-config.ts
@@ -54,14 +54,14 @@ const config: WebsiteConfig = {
   coverImage: 'img/blog-cover.png',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://beanba-blog.netlify.app/',
+  siteUrl: 'https://beanba.site/',
   // facebook: 'https://www.facebook.com/ghost',
   // twitter: 'https://twitter.com/tryghost',
   showSubscribe: false,
-  mailchimpAction:
-    'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
-  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
-  mailchimpEmailFieldName: 'MERGE0',
+  // mailchimpAction:
+  //   'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  // mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  // mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
   footer: '배운 것을 기록하고 공유합니다.',
 };
