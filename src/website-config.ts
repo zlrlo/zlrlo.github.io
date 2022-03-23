a69b28489edf412f3f2d2cc1a21c2aa4f5539597
zlrlo <easy10612@gmail.com>
2022-03-24 00:19:34 +0900

    chore: footer 문구 제거

--- src/website-config.ts
+++ src/website-config.ts
@@ -63,7 +63,7 @@ const config: WebsiteConfig = {
   // mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   // mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: '배운 것을 기록하고 공유합니다.',
+  // footer: '배운 것을 기록하고 공유합니다.',
 };
 
 export default config;
